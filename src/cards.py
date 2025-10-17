89a943655df9a08635ad2ef0ffc0eb8d2320c07a
Filip <filiopeq@gmail.com>
2025-10-17 18:34:23 +0200

    hashing ready???

--- src/cards.py
+++ src/cards.py
@@ -1,5 +1,7 @@
 import numpy as np
 import random_qiskit
+import time
+import hashlib
 
 colors = ["spades", "hearts", "clubs", "diamonds"]
 numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
@@ -12,23 +14,41 @@ class Card:
 
 
 class Deck:
-    def __init__(self, num_of_decks):
+
+    current_card = 0
+
+    def __init__(self, num_of_decks, security_bit_count):
         self.num_of_decks = num_of_decks
+        self.security_bit_count = security_bit_count
         self.deck = None
 
     def generate_deck(self):
-        self.deck = np.empty(shape=(52 * self.num_of_decks), dtype=Card)
+        self.deck = np.empty(shape=((len(colors) * len(numbers)) * self.num_of_decks), dtype=Card)
 
         for i in range(self.num_of_decks):
             for j, color in enumerate(colors):
                 for k, number in enumerate(numbers):                    
-                    self.deck[i * 52 + j * len(numbers) + k] = Card(number, color)
+                    self.deck[i * (len(colors) * len(numbers)) + j * len(numbers) + k] = Card(number, color)
 
     def shuffle_deck(self):
-        qseed = random_qiskit.quantum_generate_random(8)
-        np.random.seed(int(qseed, 2))
+        self.qseed = int(random_qiskit.quantum_generate_random(bit_count=self.security_bit_count), 2)
+        np.random.seed(self.qseed)
         np.random.shuffle(self.deck)
 
     def show_deck(self):
         for i, card in enumerate(self.deck):
             print(f"{i}: {card.color} {card.number}")
+
+    def next_card(self):
+        self.current_card += 1
+        if self.current_card >= (len(colors) * len(numbers)) * self.num_of_decks:
+            return None
+
+        return self.deck[self.current_card - 1]
+    
+    def show_commitment(self):
+        timestamp = time.time_ns()
+        commitment = hashlib.sha3_512(f"{self.qseed}:{timestamp}".encode()).hexdigest()
+        print(self.qseed)
+        print(commitment)
+        return commitment
--- src/test.py
+++ src/test.py
@@ -1,7 +1,18 @@
 import cards
 import random_qiskit
 
-my_deck = cards.Deck(2)
+my_deck = cards.Deck(num_of_decks=2, security_bit_count=8)
 my_deck.generate_deck()
 my_deck.shuffle_deck()
 my_deck.show_deck()
+
+my_deck.show_commitment()
+# import hashlib, secrets, time
+
+# number = 123456789
+# timestamp = int(time.time())  # current Unix time
+# # nonce = secrets.token_hex(8)
+# commitment = hashlib.sha3_512(f"{number}:{timestamp}".encode()).hexdigest()
+# print("Commitment:", commitment)
+# print("Timestamp:", timestamp)
+# # print("Nonce:", nonce)
